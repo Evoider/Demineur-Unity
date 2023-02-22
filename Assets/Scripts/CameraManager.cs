50e1aeec7c78ca27c7f676edb18f58e34c40ce90
YRGTech <117647468+YRGTech@users.noreply.github.com>
2023-02-22 10:40:27 +0100

    deplacement camera

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Timeline;
 using UnityEngine;
 
 public class CameraManager : MonoBehaviour
@@ -7,18 +9,54 @@ public class CameraManager : MonoBehaviour
     private GameManager gameManager;
     private Camera cam;
 
+    private float horizontalInput;
+    private float verticalInput;
+    [SerializeField] private float speed = 10;
+    private float bound;
+    private bool pause;
     // Start is called before the first frame update
     void Start()
     {
         cam = GetComponent<Camera>();
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         UpdateCamera();
+        
     }
 
+    private void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+
+        if(!pause) {
+            Vector3 movement = new(horizontalInput, verticalInput);
+
+            if (movement.magnitude > 1)
+                movement.Normalize();
+
+            transform.position += speed * Time.deltaTime * movement;
+
+            if (transform.position.x > bound - 2)
+                transform.position = new Vector3(bound - 2, transform.position.y, transform.position.z);
+            if (transform.position.x < 2)
+                transform.position = new Vector3(2, transform.position.y, transform.position.z);
+            if (transform.position.y > bound - 2)
+                transform.position = new Vector3(transform.position.x, bound - 2, transform.position.z);
+            if (transform.position.y < 2)
+                transform.position = new Vector3(transform.position.x, 2, transform.position.z);
+        }
+
+    }
     // Update is called once per frame
     public void UpdateCamera()
     {
-        transform.position = new Vector3((gameManager.width / 2) - 0.5f, (gameManager.height / 2) - 0.5f, -10);
-        cam.orthographicSize = gameManager.height / 2;
+        bound = gameManager.width;
+        transform.position = new Vector3((bound / 2) - 0.5f, (bound / 2) - 0.5f, -10);
+        cam.orthographicSize = bound / 2;
+    }
+    public void pauseCamera()
+    {
+        pause = !pause;
     }
 }
