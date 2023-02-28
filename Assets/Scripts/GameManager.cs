58b9f3a47dd7b184acb230a976438c69d5324738
YRGTech <117647468+YRGTech@users.noreply.github.com>
2023-02-28 17:46:58 +0100

    fix for build

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using TMPro;
-using Unity.VisualScripting;
-using UnityEditor.Tilemaps;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
